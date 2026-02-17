5b0c7489bd322f7f53e76a9861d8927c38e87f0d
Nikhil-Kumawat369 <nikhilkumawat6446@gmail.com>
2026-02-17 23:01:25 +0530

    intermediate commit for progress backup

---
+++ script.js
@@ -0,0 +1,11 @@
+let choices = window.document.querySelectorAll(".choice") ;
+
+choices.forEach((val) => {
+
+    val.addEventListener("click" , () => {
+
+        console.log("User Just Selected His/Her Move") ;
+
+    }) ;
+
+}) ;
